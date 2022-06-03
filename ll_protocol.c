faea3609a6312150209a6b0c7243c52b6c497a64
NazarLozynskyi <lozynskyi.nazar@gmail.com>
2022-06-03 12:10:21 +0300

    rewrite without redefining and without callbacks

--- ll_protocol.c
+++ ll_protocol.c
@@ -1,15 +1,19 @@
 #include "ll_protocol.h"
 
 
-size_t ll_sizeof_serialized(uint8_t* const data)
+size_t ll_sizeof_serialized(ll_message_info_t msg_info, const uint8_t* data)
 {
-    //+2 is for BEGIN_BYTE at the beginning and END_BYTE at the end of message
-    size_t result = MESSAGE_SIZE + 2;
-    for(size_t i = 0; i < MESSAGE_SIZE; i++)
+    if(!data)
     {
-        if(   data[i] == BEGIN_BYTE
-           || data[i] == END_BYTE
-           || data[i] == REJECT_BYTE)
+        return 0;
+    }
+    //+2 is for msg_info.begin_byte at the beginning and msg_info.end_byte at the end of message
+    size_t result = msg_info.size + 2;
+    for(size_t i = 0; i < msg_info.size; i++)
+    {
+        if(   data[i] == msg_info.begin_byte
+           || data[i] == msg_info.end_byte
+           || data[i] == msg_info.reject_byte)
         {
             result++;
         }
@@ -17,25 +21,25 @@ size_t ll_sizeof_serialized(uint8_t* const data)
     return result;
 }
 
-bool ll_serialize(uint8_t* const data_in, uint8_t* const data_out)
+void ll_serialize(ll_message_info_t msg_info, const uint8_t* data_in, uint8_t* data_out)
 {
     if(!data_in || !data_out)
     {
-        return false;
+        return;
     }
 
     uint8_t* tmp_out = data_out;
 
-    *data_out = BEGIN_BYTE;
+    *data_out = msg_info.begin_byte;
     tmp_out++;
 
-    for(size_t i = 0; i < MESSAGE_SIZE; i++)
+    for(size_t i = 0; i < msg_info.size; i++)
     {
-        if(   data_in[i] == BEGIN_BYTE
-           || data_in[i] == END_BYTE
-           || data_in[i] == REJECT_BYTE)
+        if(   data_in[i] == msg_info.begin_byte
+           || data_in[i] == msg_info.end_byte
+           || data_in[i] == msg_info.reject_byte)
         {
-            *tmp_out = REJECT_BYTE;
+            *tmp_out = msg_info.reject_byte;
             tmp_out++;
             *tmp_out = data_in[i];
             tmp_out++;
@@ -46,31 +50,27 @@ bool ll_serialize(uint8_t* const data_in, uint8_t* const data_out)
             tmp_out++;
         }
     }
-    *tmp_out = END_BYTE;
-    return true;
+    *tmp_out = msg_info.end_byte;
 }
 
-bool ll_deserialize(uint8_t* const byte_stream,
-                    size_t byte_stream_size,
-                    size_t* const start_of_remainder,
-                    void* context,
-                    void (*data_ready_cb)(uint8_t* message, void* context),
-                    void (*error_cb)(ll_protocol_err_t error, void* context))
+ll_status_t ll_deserialize(ll_message_info_t msg_info,
+                           const uint8_t* byte_stream, 
+                           size_t byte_stream_size, 
+                           uint8_t* data_out,
+                           size_t* remainder)
 {
-    if(byte_stream == NULL || start_of_remainder == NULL)
+    if(!byte_stream || !data_out || !remainder)
     {
-        return false;
+        return LL_STATUS_BAD_PARAMS;
     }
 
-    uint8_t message[MESSAGE_SIZE];
-
-    *start_of_remainder = 0;
+    *remainder = 0;
     bool message_opened = false;
     bool reject = false;
     bool ignore_previous = false;
     size_t message_iter = 0;
-    uint8_t byte = END_BYTE;
-    uint8_t previous_byte = END_BYTE;
+    uint8_t byte = msg_info.end_byte;
+    uint8_t previous_byte = msg_info.end_byte;
 
     for(size_t i = 0; i < byte_stream_size; i++)
     {
@@ -78,8 +78,8 @@ bool ll_deserialize(uint8_t* const byte_stream,
         byte = byte_stream[i];
 
         if(  !message_opened
-           && byte == BEGIN_BYTE
-           && previous_byte != REJECT_BYTE)
+           && byte == msg_info.begin_byte
+           && previous_byte != msg_info.reject_byte)
         {
             message_opened = true;
         }
@@ -89,57 +89,55 @@ bool ll_deserialize(uint8_t* const byte_stream,
             continue;
         }
 
-        if(message_iter == MESSAGE_SIZE)
+        if(message_iter == msg_info.size)
         {
             message_opened = false;
-            if(byte == END_BYTE)
+            if(byte == msg_info.end_byte)
             {
-                if(data_ready_cb)
+                if(i == byte_stream_size - 1)
                 {
-                    (data_ready_cb)(message, context);
+                    *remainder = 0;
                 }
-                *start_of_remainder = i + 1;
+                else
+                {
+                    *remainder = i + 1;
+                }
+                return LL_STATUS_SUCCESS;
             }
             else
             {
-                if(error_cb)
-                {
-                    (error_cb)(LL_PROTOCOL_ERR_MESSAGE_TOO_LONG, context);
-                }
-                *start_of_remainder = i;
+                *remainder = i;
+                return LL_STATUS_MESSAGE_TOO_LONG;
             }
             message_iter = 0;
             continue;
         }
 
-        if(   byte == END_BYTE
-           && previous_byte != REJECT_BYTE
+        if(   byte == msg_info.end_byte
+           && previous_byte != msg_info.reject_byte
            && !ignore_previous
-           && message_iter < MESSAGE_SIZE)
+           && message_iter < msg_info.size)
         {
             message_opened = false;
             reject = false;
             message_iter = 0;
-            *start_of_remainder = i + 1;
-            if(error_cb)
-            {
-                (error_cb)(LL_PROTOCOL_ERR_MESSAGE_TOO_SHORT, context);
-            }
+            *remainder = i + 1;
+            return LL_STATUS_MESSAGE_TOO_SHORT;
         }
 
         //it seems that it can be optimized
         //by combining this "if()"
-        if(   (byte != REJECT_BYTE
-           && byte != BEGIN_BYTE
-           && byte != END_BYTE
-           && previous_byte != REJECT_BYTE)
+        if(   (byte != msg_info.reject_byte
+           && byte != msg_info.begin_byte
+           && byte != msg_info.end_byte
+           && previous_byte != msg_info.reject_byte)
            ||
-             (byte != REJECT_BYTE
-           && byte != BEGIN_BYTE
-           && byte != END_BYTE
+             (byte != msg_info.reject_byte
+           && byte != msg_info.begin_byte
+           && byte != msg_info.end_byte
            && ignore_previous))
         {
-            message[message_iter++] = byte;
+            data_out[message_iter++] = byte;
             ignore_previous = false;
             continue;
          }
@@ -147,8 +145,8 @@ bool ll_deserialize(uint8_t* const byte_stream,
         //with this "if()"
         if(reject)
         {
-            message[message_iter++] = byte;
-            if(byte == REJECT_BYTE)
+            data_out[message_iter++] = byte;
+            if(byte == msg_info.reject_byte)
             {
                 ignore_previous = true;
             }
@@ -156,7 +154,7 @@ bool ll_deserialize(uint8_t* const byte_stream,
             continue;
         }
 
-        if(byte == REJECT_BYTE && (previous_byte != REJECT_BYTE || ignore_previous))
+        if(byte == msg_info.reject_byte && (previous_byte != msg_info.reject_byte || ignore_previous))
         {
             reject = true;
             if(ignore_previous)
@@ -166,5 +164,12 @@ bool ll_deserialize(uint8_t* const byte_stream,
         }
     }
 
-    return true;
+    if(message_opened)
+    {
+        *remainder = byte_stream_size - message_iter - 1;
+        return LL_STATUS_NO_ENOUGH_BYTES;
+    }
+
+    *remainder = byte_stream_size;
+    return LL_STATUS_NO_MESSAGE;
 }
--- ll_protocol.h
+++ ll_protocol.h
@@ -1,18 +1,17 @@
 /*
-    This code is written in C language to make it portable.
-Purpose of this code is to catch separate messages from the
+    Purpose of this code is to catch separate messages from the
 bytes stream. That is done by serializing message on the
 transmitter node and deserializing bytes stream on the receiver node.
 Serializing means adding some extra bytes to message which
 help to detect begin and the end of message in the bytes stream. These
-bytes are BEGIN_BYTE, END_BYTE and REJECT_BYTE. They are called
-"control bytes". BEGIN_BYTE is added at the beginning of message,
-END_BYTE at the end of message, REJECT_BYTE is used to differentiate
+bytes are "begin byte", "end byte" and "reject byte". They are called
+"control bytes". "begin byte" is added at the beginning of message,
+"end byte" at the end of message, "reject byte" is used to differentiate
 control bytes from message bytes.
 
     You can define control bytes as you want but they must be
-different from each other. For example, BEGIN_BYTE can be equal to
-0x56, END_BYTE 0x65 and REJECT_BYTE 0xFF and so on. Defining
+different from each other. For example, "begin byte" can be equal to
+0x56, "end byte" 0x65 and "reject byte" 0xFF and so on. Defining
 your own values for control bytes can be useful when the message
 that you must serialize contains a lot of bytes which values are
 the same as values of control bytes. That means that you can
@@ -21,11 +20,11 @@ examples below.
 
     NOTE. There is also control of message size. That means that
 you can't serialize and deserialize a message with size (quantity of bytes)
-that is not equal to MESSAGE_SIZE. MESSAGE_SIZE can be defined by you.
-MESSAGE_SIZE must be equal both on transmitter and receiver nodes.
+that is not equal to message size. Message size can be defined by you.
+Message size must be equal both on transmitter and receiver nodes.
 
-Examples (for MESSAGE_SIZE 16 bytes, BEGIN_BYTE 0xAA, END_BYTE 0xBB
-and REJECT_BYTE 0xCC):
+Examples (for message size 16 bytes, "begin byte" 0xAA, "end byte" 0xBB
+and "reject byte" 0xCC):
 
 1. Message that we need to serialize and transmit:
    F3 77 56 C4 95 94 76 8B 12 88 34 DD 44 77 51 31
@@ -34,32 +33,32 @@ and REJECT_BYTE 0xCC):
    AA F3 77 56 C4 95 94 76 8B 12 88 34 DD 44 77 51 31 BB
 
    As you can see, resulting bytes stream that will be transmitted
-   contains 18 bytes. More than MESSAGE_SIZE. So, the rendundancy for
-   the message in this example is 2 bytes. As you can see BEGIN_BYTE
-   added at the beginning of message and END_BYTE at the end of message.
+   contains 18 bytes. More than message size. So, the rendundancy for
+   the message in this example is 2 bytes. Also you can see that "begin byte"
+   added at the beginning of message and "end byte" at the end of message.
 
 2. Message that we need to serialize and transmit (mention that
-   second byte has the same value as END_BYTE):
+   second byte has the same value as "end byte"):
    F3 BB 56 C4 95 94 76 8B 12 88 34 DD 44 77 51 31
 
    Bytes stream after serializing:
    AA F3 CC BB 56 C4 95 94 76 8B 12 88 34 DD 44 77 51 31 BB
 
-   Resulting bytes stream is 19 bytes. Rendundancy is 3 bytes. REJECT_BYTE
+   Resulting bytes stream is 19 bytes. Rendundancy is 3 bytes. "reject byte"
    is added before second byte of message to inform deserializator that
-   this byte is not control byte. Without REJECT_BYTE deserializator
+   this byte is not control byte. Without "reject byte" deserializator
    would interpret that byte as the end of message.
 
 3. Message that we need to serialize and transmit (mention that
-   second byte has the same value as REJECT_BYTE):
+   second byte has the same value as "reject byte"):
    F3 CC 56 C4 95 94 76 8B 12 88 34 DD 44 77 51 31
 
    Bytes stream after serializing:
    AA F3 CC CC 56 C4 95 94 76 8B 12 88 34 DD 44 77 51 31 BB
 
-   Resulting bytes stream is 19 bytes. Rendundancy is 3 bytes. REJECT_BYTE
+   Resulting bytes stream is 19 bytes. Rendundancy is 3 bytes. "reject byte"
    is added before second byte of message to inform deserializator that
-   this byte is not control byte. Without REJECT_BYTE deserializator
+   this byte is not control byte. Without "reject byte" deserializator
    would interpret that byte as control byte.
 
 4. Other examples.
@@ -85,14 +84,14 @@ and REJECT_BYTE 0xCC):
 
    As you can see, the more bytes in message, which values are equal to values
 of control bytes, the more redundancy it causes. The redundancy can't be more
-than MESSAGE_SIZE * 2 + 2 (examples 4.2, 4.3, 4.4).
+than message size * 2 + 2 (examples 4.2, 4.3, 4.4).
 
 WARNING. There is one nuance. Imagine that we trying to send two messages in serial:
 MESSAGE ONE: DD DD DD DD DD DD CC DD DD DD DD DD
 MESSAGE TWO: DD DD DD DD DD DD DD DD DD DD DD DD
 Result bytes stream: AA DD DD DD DD DD DD CC CC DD DD DD DD DD BB AA DD DD DD DD DD DD DD DD DD DD DD DD BB
 
-Than imagine situation:
+And now imagine situation:
 Transmitter is sending AA DD DD DD DD DD DD CC -> some distortion happened -> transmitter start sending next message.
 
 So we get such (from the point of view of the receiver) bytes stream:
@@ -103,156 +102,122 @@ start byte of second message will be interpreted as byte that must be rejected.
 
 
 Example for code use:
-
-//define callback
-void ll_message_ready_cb(uint8_t* const message)
-{
-    printf("ready: ");
-    for(size_t i = 0; i < MESSAGE_SIZE; i++)
-    {
-        printf("%02X ", message[i]);
-    }
-    printf("\n");
-}
-
-//define callback
-void ll_error_cb(ll_protocol_err_t error)
-{
-    if(error == LL_PROTOCOL_ERR_MESSAGE_TOO_SHORT)
-    {
-        printf("LL_PROTOCOL_ERR_MESSAGE_TOO_SHORT\n");
-    }
-    if(error == LL_PROTOCOL_ERR_MESSAGE_TOO_LONG)
-    {
-        printf("LL_PROTOCOL_ERR_MESSAGE_TOO_LONG\n");
-    }
-}
-
-void transmitter_node()
-{
-    uint8_t data[MESSAGE_SIZE] =
-    {
-        0xCC, 0xCC, 0xDD, 0xDD,
-        0xDD, 0xDD, 0xDD, 0xDD,
-        0xDD, 0xDD, 0xDD, 0xDD,
-        0xDD, 0xDD, 0xDD, 0xDD
-    };
-
-    size_t byte_stream_size = ll_sizeof_serialized(data);
-    uint8_t* byte_stream = (uint8_t*)malloc(byte_stream_size);
-    ll_serialize(data, byte_stream);
-
-    transmit(byte_stream, byte_stream_size);
-}
-
-void receiver_node(uint8_t* byte_stream, size_t byte_stream_size)
-{
-    size_t _remainder;
-    ll_deserialize(byte_stream, byte_stream_size, &_remainder);
-    //or you can implement calling of ll_deserialize in loop
-}
+@todo
 
 */
 
 #ifndef LL_PROTOCOL_H
 #define LL_PROTOCOL_H
 
-#include <stdint.h>
-#include <stdlib.h>
-#include <stdbool.h>
-
 #ifdef __cplusplus
 extern "C" {
 #endif // __cplusplus
 
-#define MESSAGE_SIZE          (size_t)2
-#define MAX_BYTE_STREAM_SIZE  (size_t)(MESSAGE_SIZE * 2 + 2)
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdbool.h>
 
-#define BEGIN_BYTE    (uint8_t)0xAA
-#define END_BYTE      (uint8_t)0xBB
-#define REJECT_BYTE   (uint8_t)0xCC
 
 typedef enum
 {
-    LL_PROTOCOL_ERR_MESSAGE_TOO_SHORT, //message has started with BEGIN_BYTE but has ended too early with END_BYTE
-    LL_PROTOCOL_ERR_MESSAGE_TOO_LONG   //message started with BEGIN_BYTE but hasn't end with END_BYTE after last byte of message came
-} ll_protocol_err_t;
+    LL_STATUS_SUCCESS,           //success
+    LL_STATUS_BAD_PARAMS,        //bad parameters
+    LL_STATUS_NO_MESSAGE,        //there are no message in byte stream
+    LL_STATUS_NO_ENOUGH_BYTES,   //byte stream end in the middle of message
+    LL_STATUS_MESSAGE_TOO_SHORT, //message has started with "begin byte" but has ended too early with "end byte"
+    LL_STATUS_MESSAGE_TOO_LONG,  /*message started with "begin byte" but hasn't end with "end byte" after last 
+                                   byte of message came*/
+    LL_STATUS_ENUM_SIZE          //enum size
+} ll_status_t;
 
+typedef struct
+{
+    size_t  size;        //message size
+    uint8_t begin_byte;  //begin byte
+    uint8_t reject_byte; //reject byte
+    uint8_t end_byte;    //end byte
+} ll_message_info_t;
 
-/*
- * This function is used to know how many bytes you need
- * to reserve for serialized data.
- *
- * Returns quantity of bytes which must be reserved for "data_out"
- * in "ll_serialize".
- *
- * NOTE. "ll_sizeof_serialized" will parse area of memory
- * with size MESSAGE_SIZE to which parameter "data" points.
- */
-size_t ll_sizeof_serialized(
-    uint8_t* const data         // data which will be serialized in "ll_serialize"
-);
-
-/*
- * This function serializes "data_in" and puts the result to "data_out".
- *
- * Returns "true" on success and "false" on fail.
- *
- * NOTE. "data_in" size must be equal to MESSAGE_SIZE and "data_out"
- * size must by equal to size which was returned by ll_sizeof_serialized
- * for data_in.
- */
-bool ll_serialize(
-    uint8_t* const data_in,     // data which will be serialized
-    uint8_t* const data_out     // array where result must be put
-);
 
 /**
- * @brief This function parses bytes stream. When message is detected it
- * calls "data_ready_cb" function. When error in bytes stream
- * detected it calls "error_cb" function.
- *
- * If in bytes stream remain bytes which were not parsed - this
- * function writes in "start_of_remainder" position in "byte_stream"
- * where these bytes started. If all bytes from bytes stream were parsed
- * then "start_of_remainder" must be equal to [byte_stream_size + 1]. If
- * none of the bytes in bytes stream were parsed then "start_of_remainder"
- * equals to zero. NOTE. There can be only two situations with remainder:
- * 1) none of the bytes were parsed; 2) first bytes was parsed and last remains.
- * That means that the end of remainder always ends in [byte_stream_size - 1]
- * position and can't be less than [byte_stream_size - 1].
- *
- * Logic of remainder manipulating must be implemented by you. This function
- * only points to position of start of remainder.
- *
- * @param byte_stream byte stream which must be deserialized
- * @param byte_stream_size byte stream size
- * @param start_of_remainder position of start of remainder in "byte_stream"
- * after deserializing
- * @param context context for multitasking
- *
- * @param data_ready_cb this function is called by "ll_deserialize" every time
- * when new message is detected, if data_ready_cb == NULL - do nothing
- *
- * @param error_cb this function is called by "ll_deserialize" every time when
- * error in bytes stream but errors such as wrong bytes between messages which
- * not started with BEGIN_BYTE will not be catched, if error_cb == NULL - do nothing
- *
- * @return "true" on success and "false" on fail.
+ * @brief This function is used to know how many bytes you need
+ * to reserve for serialized data.
+ * @param msg_info message info
+ * @param data area of memory with size of msg_info.message_size which will be parsed
+ * if data == NULL then function does nothing and returns 0
+ * @returns quantity of bytes which must be reserved for "data_out" in "ll_serialize"
  */
-bool ll_deserialize(
-    uint8_t* byte_stream,
-    size_t byte_stream_size,
-    size_t* start_of_remainder,
-    void* context,
-    void (*data_ready_cb)(
-        uint8_t* message,          //incoming message to process with size of MESSAGE_SIZE
-        void* context              //context for multitasking
-    ),
-    void (*error_cb)(
-        ll_protocol_err_t error,   //type of error
-        void* context              //context for multitasking
-    )
+size_t ll_sizeof_serialized(ll_message_info_t msg_info, const uint8_t* data);
+
+/**
+ * @brief This function serializes "data_in" and puts the result to "data_out".
+ *
+ * @param msg_info message info
+ * @param data_in area of memory with size of msg_info.message_size which will be parsed, 
+ * if data_in == NULL then function does nothing
+ * @param data_out area of memory with size that was returned by ll_sizeof_serialized,
+ * if data_out == NULL then function does nothing
+ */
+void ll_serialize(ll_message_info_t msg_info, const uint8_t* data_in, uint8_t* data_out);
+
+/**
+ * @brief This function parses bytes stream and puts the result to "data_out". When 
+ * function ends parsing, it writes position of remainder to "remainder" poiner and 
+ * returns status of parsing.
+ * 
+ * There can be only six cases in function behaviour:
+ * 
+ * 1. There are no one sequence of bytes started with "begin byte". 
+ * Behaviour: function writes to "status" LL_STATUS_NO_MESSAGE and returns "byte_stream_size".
+ * 
+ * 2. There is a sequence of bytes started with "begin byte" but "end byte" come
+ * too early. In other words - message is too short. 
+ * Behaviour: function writes to "status" LL_STATUS_MESSAGE_TOO_SHORT and returns position of
+ * next byte after "end byte" of broken message.
+ * 
+ * 3. There is a sequence of bytes started with "begin byte" but "end byte" doesn't come in time. 
+ * In other words - message is too long.
+ * Behaviour: function writes to "status" LL_STATUS_MESSAGE_TOO_LONG and returns position where 
+ * "end byte" should have been.
+ * 
+ * 4. There is a sequence of bytes started with "begin byte" but in the middle of sequence 
+ * we reach the end of byte stream.
+ * Behaviour: function writes to reminder position of "start byte" of uncompleted message and
+ * returns LL_STATUS_NO_ENOUGH_BYTES.
+ * 
+ * 5. Message was normally parsed and there are remaining bytes.
+ * Behaviour: function writes to "remainder" position of next byte after "end byte" of parsed message 
+ * and returns LL_STATUS_SUCCESS.
+ * 
+ * 6. Message was normally parsed and there are no remaining bytes.
+ * Behaviour: function writes 0 to "remainder" and returns LL_STATUS_SUCCESS.
+ * 
+ * @note During parsing byte stream function writes result to "data_out". It means that
+ * "data_out" always will be modified except of first case said above.
+ * 
+ * @note Function ignores all bytes sequences between messages in byte stream. It is about bytes 
+ * sequences which are not started with "begin byte". That means that there will not be any
+ * information about these lost bytes.
+ * 
+ * @todo Catch information about lost bytes.
+ * @todo Rewrite logic to make it closer to repetitive calling of this function.
+ *
+ * @param msg_info message info
+ * @param byte_stream area of memory with size of byte_stream_size which will be parsed, 
+ * if byte_stream == NULL then function does nothing and returns LL_STATUS_BAD_PARAMS 
+ * @param byte_stream_size byte stream size
+ * @param data_out area of memory with size of msg_info.message_size where result will be putted, 
+ * if data_out == NULL then function does nothing and returns LL_STATUS_BAD_PARAMS
+ * @param remainder pointer to remainder,
+ * if remainder == NULL then function does nothing and returns LL_STATUS_BAD_PARAMS
+ * @returns status
+ */
+ll_status_t ll_deserialize(
+    ll_message_info_t msg_info,
+    const uint8_t* byte_stream, 
+    size_t byte_stream_size, 
+    uint8_t* data_out,
+    size_t* remainder
 );
 
 #ifdef __cplusplus
